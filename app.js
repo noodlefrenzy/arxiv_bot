035c114d0be7632c7415833af73834cd95dd77a0
Michael Lanzetta <milanz@microsoft.com>
2016-08-06 17:55:23 -0700

    More testing for skype bot

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ function cardForPaper(session, paper) {
     var paperCard = new builder.HeroCard(session);
     paperCard.title = paper.title;
     paperCard.subtitle = util.format('ID: %s, Version: %s, Published: %s', paper.id, paper.version, paper.pubdate);
-    paperCard.text = util.format('<a href="%s">%s</a>', paper.uri, paper.title);
+    paperCard.text = util.format('<a href="%s">%s</a><br/><div style="font-size: 75%">%s</div>', paper.uri, paper.title, paper.summary);
     paperCard.tap = builder.CardAction.openUrl(session, paper.uri);
     return paperCard;
 }
